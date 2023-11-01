0bc2af6f0df84df5d7e5fc7fde6d68aceb114d25
vidhan <128739405+PUNEETRKRAJPUT@users.noreply.github.com>
2023-11-01 13:53:37 +0530

    commit

--- Invoice.php
+++ Invoice.php
@@ -87,8 +87,8 @@ include('config/config.php');
                                     Purchase
                                 </th>
 
-                                <th scope="col" class="relative py-3.5 px-4">
-                                    <span class="sr-only">Actions</span>
+                                <th scope="col" class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                    Actions
                                 </th>
                             </tr>
                         </thead>
@@ -108,10 +108,10 @@ include('config/config.php');
                                     <div class="inline-flex items-center gap-x-3">
                                         <input type="checkbox" class="text-blue-500 border-gray-300 rounded dark:bg-gray-900 dark:ring-offset-gray-900 dark:border-gray-700">
 
-                                        <span>#<?php echo $row[ID]; ?></span>
+                                        <span>#<?php echo $row['ID']; ?></span>
                                     </div>
                                 </td>
-                                <td class="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap"><?php echo $row[Date]; ?></td>
+                                <td class="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap"><?php echo $row['Date']; ?></td>
                                 <td class="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
                                     <div class="inline-flex items-center px-3 py-1 rounded-full gap-x-2 text-emerald-500 bg-emerald-100/60 dark:bg-gray-800">
                                         <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -125,8 +125,8 @@ include('config/config.php');
                                     <div class="flex items-center gap-x-2">
                                         <img class="object-cover w-8 h-8 rounded-full" src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt="">
                                         <div>
-                                            <h2 class="text-sm font-medium text-gray-800 dark:text-white "><?php echo $row[Name]; ?></h2>
-                                            <p class="text-xs font-normal text-gray-600 dark:text-gray-400"><?php echo $row[Email]; ?></p>
+                                            <h2 class="text-sm font-medium text-gray-800 dark:text-white "><?php echo $row['Name']; ?></h2>
+                                            <p class="text-xs font-normal text-gray-600 dark:text-gray-400"><?php echo $row['Email']; ?></p>
                                         </div>
                                     </div>
                                 </td>
@@ -134,7 +134,7 @@ include('config/config.php');
                                 <td class="px-4 py-4 text-sm whitespace-nowrap">
                                     <div class="flex items-center gap-x-6">
                                         <form action="Invoice-View.php" method="post">
-                                            <button type="submit" name="view-invoice" value="<?php echo $row[ID]; ?>" class="text-gray-500 transition-colors duration-200 dark:hover:text-indigo-500 dark:text-gray-300 hover:text-indigo-500 focus:outline-none">
+                                            <button type="submit" name="view-invoice" value="<?php echo $row['ID']; ?>" class="text-gray-500 transition-colors duration-200 dark:hover:text-indigo-500 dark:text-gray-300 hover:text-indigo-500 focus:outline-none">
                                                 View
                                             </button>
                                         </form>
