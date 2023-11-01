77fa781ac6cc8c82ceec0e886b33b849de2c1cb7
vidhan <128739405+PUNEETRKRAJPUT@users.noreply.github.com>
2023-11-01 13:29:03 +0530

    commit

---
+++ Invoice-Form.php
@@ -0,0 +1,60 @@
+<?php 
+session_start();
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site</title>
+<style>
+@keyframes gradientAnimation {
+    0% {
+        background: linear-gradient(45deg, #4d9af3, #ff9900, #4adb4a, #3399ff, #cc66ff);
+        background-size: 600% 100%;
+        background-position: 0% 100%;
+    }
+    100% {
+        background: linear-gradient(45deg, #cc66ff, #3399ff, #5ce25c, #ff9900);
+        background-size: 600% 100%;
+        background-position: 100% 0%;
+    }
+}
+
+.bg-gradient-animation {
+    animation: gradientAnimation 10s linear infinite;
+}
+
+</style>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+</head>
+<body>
+
+<script src="https://cdn.tailwindcss.com"></script>
+<?php include 'Header.php';?>
+<section>
+    <div class="flex flex-row">
+        <div class="w-1/4">
+            <?php include('Sidebar.php');?>
+        </div>
+        <div class="w-2/3">
+
+        <h1 class="font-semibold text-center text-2xl my-5">Generate Invoice</h1>
+        
+        
+
+
+
+
+
+        </div>
+    </div>
+</section>
+
+
+<?php include 'footer.php';?>
+</body>
+</html>
